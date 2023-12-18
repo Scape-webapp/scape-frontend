3503874a04794c5a7edef5c9444808846389b35e
yashbhanu <bhanushaliyash2000@gmail.com>
2023-12-18 23:25:39 +0530

    socket connection established for sending & recieving msgs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,72 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import socketIOClient, { Socket } from "socket.io-client";
 
 export default function Home() {
+  let socket: Socket;
+  const [userId, setuserId] = useState("");
+  const [message, setMessage] = useState("");
+
+  const joinChat = async () => {
+    socket = socketIOClient("http://localhost:5000", {
+      reconnectionDelay: 1000,
+      reconnection: true,
+      // reconnectionAttemps: 10,
+      transports: ["websocket"],
+      agent: false,
+      upgrade: false,
+      rejectUnauthorized: false,
+    });
+
+    socket.on("msg-recieve", (data) => {
+      debugger;
+      console.log("object :>> ", data);
+    });
+  };
+
+  const addUser = async () => {
+    await joinChat();
+    socket.emit("add-user", {
+      id: userId,
+    });
+  };
+
+  const sendMessage = () => {
+    socket.emit("send-msg", {
+      reciever: "65742f91faf66f82c43d4269",
+      sender: userId,
+      text: "tune bola toh galat hi hoga 2",
+    });
+  };
+
+  useEffect(() => {
+    // joinChat();
+    return () => {
+      if (socket) {
+        socket.removeAllListeners();
+        socket.disconnect();
+      }
+    };
+  }, []);
+
   return (
     <div>
-      <p>Welcome to Scape</p>
+      <p>Login</p>
+      <label>UserName</label>
+      <input
+        type="text"
+        placeholder="userid"
+        onChange={(e) => setuserId(e.target.value)}
+      />
+      <button type="submit" onClick={addUser}>
+        Submit
+      </button>
+
+      <label>Message</label>
+      <textarea onChange={(e) => setMessage(e.target.value)}></textarea>
+      <button onClick={sendMessage}>Send Message</button>
     </div>
   );
 }
