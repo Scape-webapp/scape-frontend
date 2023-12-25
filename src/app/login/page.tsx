9de94933770a0e4e9de3540e37fc84ce518f0783
Komal Thakur <komalthakur100400@gmail.com>
2023-12-26 00:38:18 +0530

    responsive bug fixed

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -27,14 +27,14 @@ export default function Login() {
       <div className="flex items-center justify-center absolute lg:grid lg:grid-cols-2 lg:gap-x-10 w-full h-screen ">
         <div></div>
 
-        <div className="border-2 rounded-[17px] sm:h-[35rem] lg:h-fit md:p-4 bg-[#FCF7F8] w-5/6 md:w-3/5 lg:w-fit  place-self-center ">
+        <div className="border-2 rounded-[17px] sm:h-[35rem] lg:h-fit md:p-4 bg-[#FCF7F8] w-5/6 md:w-3/5 lg:w-1/3 xl:w-3/6 place-self-center ">
           <p className="text-center text-xl pt-8 pb-6 text-[#262E35] ">
             Login Page
           </p>
           <p className="text-[#787E83] text-center text-sm py-4">
             Welcome back please login to your account
           </p>
-          <div className="mx-4 md:mx-8">
+          <div className="mx-4 md:mx-6 lg:mx-8">
             <Formik
               enableReinitialize={true}
               initialValues={formValues}
@@ -46,7 +46,7 @@ export default function Login() {
             >
               {(props) => (
                 <Form onSubmit={props.handleSubmit}>
-                  <div className="grid md:px-2">
+                  <div className="grid">
                     <label className="text-[#787E83] py-2" htmlFor="UserName">
                       User Name
                     </label>
