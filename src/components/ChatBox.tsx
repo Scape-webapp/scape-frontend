86a4532cc0f20082cdea52834fab19f8205bd48e
yashbhanu <bhanushaliyash2000@gmail.com>
2023-12-30 02:55:30 +0530

    api for fetch messages added with time

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -1,27 +1,105 @@
 import { faEllipsisV, faFaceSmile } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+// var ObjectID = require("bson-objectid");
+import axios from "axios";
+import moment from "moment-timezone";
 
 export default function ChatBox() {
   const dummyMessages = [
     {
       msg: "hello",
-      recieved: "me",
-      sent: "other",
+      receiver: "me",
+      sent: "6574b5dcb558663da9b3e808",
     },
-    { msg: "hello hi", recieved: "me", sent: "other", lastMsg: true },
+    {
+      msg: "hello hi",
+      receiver: "me",
+      sent: "6574b5dcb558663da9b3e808",
+      lastMsg: true,
+    },
     {
       msg: "i am fine, hi, am fine, hi, am fine, hi lorem wywybx syx sy yazyaay twvsv",
-      recieved: "other",
+      receiver: "6574b5dcb558663da9b3e808",
       sent: "me",
     },
-    { msg: "how doin", recieved: "other", sent: "me", lastMsg: true },
-    { msg: "fantastic", recieved: "me", sent: "other" },
-    { msg: "hello hi", recieved: "me", sent: "other", lastMsg: true },
-    { msg: "i am fine, hi", recieved: "other", sent: "me" },
-    { msg: "how doin", recieved: "other", sent: "me", lastMsg: true },
-    { msg: "fantastic", recieved: "me", sent: "other", lastMsg: true },
+    {
+      msg: "how doin",
+      receiver: "6574b5dcb558663da9b3e808",
+      sent: "me",
+      lastMsg: true,
+    },
+    { msg: "fantastic", receiver: "me", sent: "6574b5dcb558663da9b3e808" },
+    {
+      msg: "hello hi",
+      receiver: "me",
+      sent: "6574b5dcb558663da9b3e808",
+      lastMsg: true,
+    },
+    { msg: "i am fine, hi", receiver: "6574b5dcb558663da9b3e808", sent: "me" },
+    {
+      msg: "how doin",
+      receiver: "6574b5dcb558663da9b3e808",
+      sent: "me",
+      lastMsg: true,
+    },
+    {
+      msg: "fantastic",
+      receiver: "me",
+      sent: "6574b5dcb558663da9b3e808",
+      lastMsg: true,
+    },
   ];
+
+  let sender = "6574bd61378887aeab034740";
+  let reciever = "6574b5dcb558663da9b3e808";
+  const [message, setmessage] = useState<any>([]);
+
+  const getMSgs = async () => {
+    const msgs = await axios.post(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/message/history`,
+      {
+        receiver: [reciever],
+        sender: sender,
+      }
+    );
+    setmessage(msgs.data);
+    // debugger;
+    let rawMsgs = [...msgs.data];
+    // const updateMsgsBody = [];
+    // rawMsgs.unshift({
+    //   date: new Date(rawMsgs[0].createdAt).toLocaleDateString(),
+    // });
+
+    // for (let i = 1; i < msgs.data.length; i++) {
+    //   // debugger;
+    //   const msgDate: string = new Date(
+    //     msgs.data[i].createdAt
+    //   ).toLocaleDateString();
+    //   const prevDate = new Date(
+    //     msgs.data[i - 1].createdAt
+    //   ).toLocaleDateString();
+    //   const todayDate: string = new Date().toLocaleDateString();
+    //   console.log('todayDate :>> ', todayDate);
+    //   if (msgDate !== prevDate) {
+    //     rawMsgs.splice(i, 0, { date: msgDate });
+    //   } else if (msgDate === todayDate) {
+    //     debugger
+    //     rawMsgs.splice(i, 0, { date: "Today" });
+    //   }
+    // }
+    // rawMsgs = [...rawMsgs];
+    console.log(
+      "moment.utc().toLocaleString() :>> ",
+      moment().tz("Asia/Calcutta").format("MMM Do YY").toLocaleString()
+    );
+    console.log(rawMsgs);
+  };
+
+  useEffect(() => {
+    const res = getMSgs();
+  }, []);
 
   return (
     <div className="max-h-screen w-full bg-[#262E35] flex flex-col">
@@ -43,55 +121,97 @@ export default function ChatBox() {
 
       <div className="h-[80%] border-b overflow-x-auto border-[#36404A] p-6">
         <div className="h-full w-full overflow-y-scroll overflow-x-hidden">
-          {/* Date */}
-          <div className="flex items-center mb-3 justify-center">
-            <div className="py-1 px-3 flex rounded justify-center bg-[#36404A]">
-              <p className="text-xs text-white">Today</p>
-            </div>
-          </div>
           {/* Messages */}
-          {dummyMessages.map((msg, i) => (
-            <div
-              key={i}
-              className={`flex gap-2 mx-6 my-4 justify-${
-                msg.recieved === "other" ? "start" : "end"
-              }`}
-              style={
-                msg.recieved === "other"
-                  ? { justifyContent: "flex-start" }
-                  : { justifyContent: "flex-end" }
-              }
-            >
-              <div className="flex gap-2">
-                {msg.lastMsg && msg.recieved === "other" ? (
-                  <Image
-                    src="/images/profile-dummy.svg"
-                    alt="profile"
-                    height={30}
-                    width={30}
-                  />
-                ) : (
-                  <div className="h-8 w-8"></div>
-                )}
-                <div
-                  className={`p-3 max-w-sm rounded-t-lg ${
-                    msg.recieved === "other"
-                      ? "bg-[#36404A] rounded-br-lg"
-                      : "bg-[#7083FF] rounded-bl-lg"
-                  }`}
-                >
-                  <p className="text-white text-base">{msg.msg}</p>
+          {message.map((msg: any, i: any) => (
+            <div key={i}>
+              {moment
+                .utc(msg.createdAt)
+                .format("MMM Do YY")
+                .toLocaleString() ===
+                moment()
+                  .tz("Asia/Calcutta")
+                  .format("MMM Do YY")
+                  .toLocaleString() &&
+              moment.utc(msg.createdAt).format("LL").toLocaleString() !==
+                moment
+                  .utc(message[i - 1]?.createdAt)
+                  .format("LL")
+                  .toLocaleString() ? (
+                <div className="flex items-center mb-3 justify-center">
+                  <div className="py-1 px-3 flex rounded justify-center bg-[#36404A]">
+                    <p className="text-sm text-white">Today</p>
+                  </div>
                 </div>
-                {msg.lastMsg && msg.sent === "other" ? (
-                  <Image
-                    src="/images/profile-dummy.svg"
-                    alt="profile"
-                    height={30}
-                    width={30}
-                  />
+              ) : moment.utc(msg.createdAt).toLocaleString() !==
+                  moment.utc(message[i - 1]?.createdAt).toLocaleString() &&
+                moment.utc(msg.createdAt).format("LL").toLocaleString() !==
+                  moment().tz("Asia/Calcutta").format("LL").toLocaleString() ? (
+                moment.utc(msg.createdAt).format("LL") ===
+                moment().subtract(1, "day").format("LL") ? (
+                  <div className="flex items-center mb-3 justify-center">
+                    <div className="py-1 px-3 flex rounded justify-center bg-[#36404A]">
+                      <p className="text-sm text-white">Yesterday</p>
+                    </div>
+                  </div> // <div>{new Date(msg.createdAt).toLocaleDateString()}</div>
                 ) : (
-                  <div className="h-8 w-8"></div>
-                )}
+                  <div className="flex items-center mb-3 justify-center">
+                    <div className="py-1 px-3 flex rounded justify-center bg-[#36404A]">
+                      <p className="text-sm text-white">
+                        {moment
+                          .utc(msg.createdAt)
+                          .format("LL")
+                          .toLocaleString()}
+                      </p>
+                    </div>
+                  </div>
+                )
+              ) : (
+                <div></div>
+              )}
+              <div
+                className={`flex gap-2 mx-6 my-4 justify-${
+                  msg.receiver.includes("6574b5dcb558663da9b3e808")
+                    ? "start"
+                    : "end"
+                }`}
+                style={
+                  msg.receiver.includes("6574b5dcb558663da9b3e808")
+                    ? { justifyContent: "flex-start" }
+                    : { justifyContent: "flex-end" }
+                }
+              >
+                <div className="flex gap-2">
+                  {msg.lastMsg &&
+                  msg.receiver.includes("6574b5dcb558663da9b3e808") ? (
+                    <Image
+                      src="/images/profile-dummy.svg"
+                      alt="profile"
+                      height={30}
+                      width={30}
+                    />
+                  ) : (
+                    <div className="h-8 w-8"></div>
+                  )}
+                  <div
+                    className={`p-3 max-w-sm rounded-t-lg ${
+                      msg.receiver.includes("6574b5dcb558663da9b3e808")
+                        ? "bg-[#36404A] rounded-br-lg"
+                        : "bg-[#7083FF] rounded-bl-lg"
+                    }`}
+                  >
+                    <p className="text-white text-base">{msg.text}</p>
+                  </div>
+                  {msg.lastMsg && msg.sender === "6574b5dcb558663da9b3e808" ? (
+                    <Image
+                      src="/images/profile-dummy.svg"
+                      alt="profile"
+                      height={30}
+                      width={30}
+                    />
+                  ) : (
+                    <div className="h-8 w-8"></div>
+                  )}
+                </div>
               </div>
             </div>
           ))}
